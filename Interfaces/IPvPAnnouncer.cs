c78d1bbb87df709208e39c086aea5fa9c8b4b89e
Spider <29214314+spide-r@users.noreply.github.com>
2025-04-13 16:45:17 -0500

    quick change in impl

--- Interfaces/IPvPAnnouncer.cs
+++ Interfaces/IPvPAnnouncer.cs
@@ -5,7 +5,6 @@ namespace PvPAnnouncer.Interfaces;
 
 public interface IPvPAnnouncer
 {
-    Hashtable AudioFiles { get; init; } //string of the path key and Scd.Audio data value
     void ReceivePvPEvent(IPvPEvent pvpEvent);
     
     void PlaySound(IPvPEvent pvpEvent);
--- Interfaces/ISoundManager.cs
+++ Interfaces/ISoundManager.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using Lumina.Data.Files;
 
 namespace PvPAnnouncer.Interfaces;
 
 public interface ISoundManager
-{
+{    
+    Hashtable AudioFiles { get; init; } //key will be path, value will be the audio data needed to play
+
     void PlaySound(ScdFile.Sound sound);
     void PlaySound(byte[] audioData);
     void PlaySound(String path);
