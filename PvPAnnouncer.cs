56d30aae85470a79ae77c9bbe8437d244a663782
Spider <29214314+spide-r@users.noreply.github.com>
2025-04-13 16:30:30 -0500

    Start of Project

---
+++ PvPAnnouncer.cs
@@ -0,0 +1,33 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Plugin;
+
+namespace PvPAnnouncer
+{
+    public sealed class PvPAnnouncer: IDalamudPlugin
+    {
+        private const string MainCommand = "/ppa";
+        private IDalamudPluginInterface PluginInterface { get; init; }
+        
+
+        public PvPAnnouncer(IDalamudPluginInterface pluginInterface)
+        {
+            Service.Initialize(pluginInterface);
+            PluginInterface = pluginInterface;
+            Service.CommandManager.AddHandler(MainCommand, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "To Be Replaced!"
+            });
+
+
+        }
+        public void Dispose()
+        {
+            Service.CommandManager.RemoveHandler(MainCommand);
+        }
+        private void OnCommand(string command, string args)
+        {
+            Service.PluginLog.Info("Hello World!");
+        }
+    }
+}
+
---
+++ Service.cs
@@ -0,0 +1,48 @@
+﻿using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace PvPAnnouncer;
+
+#pragma warning disable 8618
+internal class Service {
+
+    [PluginService]
+    internal static IDataManager DataManager { get; private set; }
+    
+    [PluginService]
+    internal static ICommandManager CommandManager { get; private set; }
+
+    [PluginService]
+    internal static IChatGui ChatGui { get; private set; }
+
+    [PluginService]
+    internal static IObjectTable ObjectTable { get; private set; }
+
+    [PluginService]
+    internal static IPartyList PartyList { get; private set; }
+
+    [PluginService]
+    internal static IClientState ClientState { get; private set; }
+    
+    [PluginService]
+    internal static IDalamudPluginInterface DalamudPluginInterface { get; private set; }
+    
+    [PluginService]
+    internal static IFramework Framework { get; private set; }
+    
+    [PluginService]
+    internal static ICondition Condition { get; private set; }
+
+    
+    [PluginService]
+    internal static IGameInteropProvider GameInteropProvider { get; private set; }
+    
+    [PluginService]
+    internal static IPluginLog PluginLog { get; private set; }
+
+    internal static void Initialize(IDalamudPluginInterface pluginInterface) {
+        pluginInterface.Create<Service>();
+    }
+}
+#pragma warning restore 8618
